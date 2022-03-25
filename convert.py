8f3f38187d5ab9ca798a5a557032610d7d6c3ada
Brian Elizondo <brian_elizondo@msn.com>
2022-03-24 21:27:48 -0400

    Step One: Working in Python Files => For all the syntax challenges work in script files to resolved

--- convert.py
+++ convert.py
@@ -1,6 +1,5 @@
 def convert_temp(unit_in, unit_out, temp):
     """Convert farenheit <-> celsius and return results.
-
     - unit_in: either "f" or "c" 
     - unit_out: either "f" or "c"
     - temp: temperature (in f or c, depending on unit_in)
@@ -10,13 +9,28 @@ def convert_temp(unit_in, unit_out, temp):
     If unit_in or unit_out are invalid, return "Invalid unit [UNIT_IN]".
 
     For example:
-
       convert_temp("c", "f", 0)  =>  32.0
       convert_temp("f", "c", 212) => 100.0
     """
 
     # YOUR CODE HERE
+    if unit_in == "c" or unit_in == "f":
+        if unit_out == "c" or unit_out == "f":
+            if unit_in == "c":
+                conversion = (temp * (5 / 9)) + 32
 
+            if unit_in == "f":
+                conversion = (temp - 32) * (5 / 9)
+
+            if unit_in == unit_out:
+                conversion = temp
+
+            return f"should be {conversion}"
+        else:
+            return f"should be Invalid unit {unit_out}"
+    else:
+        return f"should be Invalid unit {unit_in}"
+            
 
 print("c", "f", 0, convert_temp("c", "f", 0), "should be 32.0")
 print("f", "c", 212, convert_temp("f", "c", 212), "should be 100.0")
--- sum.py
+++ sum.py
@@ -12,6 +12,10 @@ def sum_nums(nums):
     # want you to use it. Please write this by hand.
 
     # YOUR CODE HERE
-
+    sum_total = 0
+    for num in nums:
+        sum_total += num
+    
+    return sum_total
 
 print("sum_nums returned", sum_nums([1, 2, 3, 4]))
